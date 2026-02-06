9bb7ab5449c8e5a3e4b3ece7c019b28640947259
Aditya-en <adityasahani443@gmail.com>
2026-02-06 12:46:43 +0530

    add icons

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,25 +20,27 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.deadline_wallpaper"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
 
-    buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
+    signingConfigs {
+        create("release") {
+            keyAlias = "upload"
+            keyPassword = "tickingapp"
+            storeFile = file("upload-keystore.jks")
+            storePassword = "tickingapp"
         }
     }
-}
 
-flutter {
-    source = "../.."
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = false
+            isShrinkResources = false
+        }
+    }
 }
